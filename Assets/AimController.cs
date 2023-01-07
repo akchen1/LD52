2c6bf14b70974d530db77d603ee8b8fe3ff31167
BhanHG <96505078+BhanHG@users.noreply.github.com>
2023-01-07 00:49:48 -0700

    Added "A" And "D" Movement And Shooting Player.

---
+++ Assets/AimController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class AimController : MonoBehaviour
+{
+    public GameObject Player; // the player GameObject
+    public GameObject Arrow;
+    public GameObject CircleOutline;
+
+    private float radius; 
+
+    void Start()
+    {
+        // Set the initial scale of the arrow
+        Arrow.SetActive(false);
+        CircleOutline.SetActive(false);
+    }
+
+    void FixedUpdate()
+    {
+
+        // Control the length of the arrow based on the left mouse button
+        if (Input.GetMouseButton(1))
+        {
+            Arrow.SetActive(true);
+            CircleOutline.SetActive(true);
+            // Rotate the arrow towards the mouse position
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float angle = Mathf.Atan2(mousePos.y - Arrow.transform.position.y, mousePos.x - Arrow.transform.position.x) * Mathf.Rad2Deg;
+            Arrow.transform.rotation = Quaternion.Euler(0, 0, angle);
+        }
+        else
+        {
+            Arrow.SetActive(false);
+            CircleOutline.SetActive(false);
+        }
+    }
+
+    public float GetRadius(){
+        return (Arrow.GetComponent<SpriteRenderer>().bounds.size.x)/2;
+    }
+
+}
