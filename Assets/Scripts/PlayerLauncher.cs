0d81cc7eaace859e775e288a2ed1bd9fe1609c5b
akchen1 <43446512+akchen1@users.noreply.github.com>
2023-01-07 17:16:59 -0700

    connect with swinging

--- Assets/Scripts/PlayerLauncher.cs
+++ Assets/Scripts/PlayerLauncher.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerLauncher : MonoBehaviour
 {
@@ -21,10 +22,14 @@ public class PlayerLauncher : MonoBehaviour
     //InitalPosition of the Player
     private Vector2 initalPosition;
 
+    private RelativeJoint2D relativeJoint;
+
     private void Start() {
         AC = this.gameObject.GetComponent<AimController>();
         radius = AC.GetRadius();
+        relativeJoint = GetComponent<RelativeJoint2D>();
     }
+
     private void Update() {
         if (Input.GetMouseButtonUp(0) && inAir == false && rightDireciton)
         {
@@ -40,27 +45,42 @@ public class PlayerLauncher : MonoBehaviour
             float distance = mouseDirection.magnitude;
 
             // Normalize the direction and scale it by the launch force
-            mouseDirection = mouseDirection.normalized;
+            mouseDirection = mouseDirection.normalized; 
             Vector2 launchVelocity = mouseDirection * launchForce;
 
             initalPosition = transform.position;
             
+            if (relativeJoint.enabled)
+            {
+                relativeJoint.connectedBody = null;
+                relativeJoint.enabled = false;
+            }
             // Apply the launch force to the Player
+            //StartCoroutine(Launch(launchVelocity));
             PlayerRigidbody.AddForce(launchVelocity, ForceMode2D.Impulse);
         }
-        if(inAir)
-        {
-            float distanceTraveled = Mathf.Sqrt(Mathf.Pow(transform.position.x - initalPosition.x, 2) + Mathf.Pow(transform.position.y - initalPosition.y, 2));  
-            if(distanceTraveled > radius){
-                Debug.Log("DEAD");
-                inAir = false;
-                PlayerRigidbody.velocity = Vector3.zero;
-                PlayerRigidbody.angularVelocity = 0;
-            }
-        }
+        //if(inAir)
+        //{
+        //    float distanceTraveled = Mathf.Sqrt(Mathf.Pow(transform.position.x - initalPosition.x, 2) + Mathf.Pow(transform.position.y - initalPosition.y, 2));  
+        //    if(distanceTraveled > radius){
+        //        Debug.Log("DEAD");
+        //        inAir = false;
+        //        PlayerRigidbody.velocity = Vector3.zero;
+        //        PlayerRigidbody.angularVelocity = 0;
+        //    }
+            
+        //}
 
 
     }
+
+    private IEnumerator Launch(Vector2 launchVelocity)
+    {
+        yield return 0;
+        PlayerRigidbody.AddForce(launchVelocity, ForceMode2D.Impulse);
+
+    }
+
     void OnCollisionStay2D(Collision2D collision)
     {
         // Get the position of the mouse in world coordinates
@@ -92,11 +112,15 @@ public class PlayerLauncher : MonoBehaviour
     {
         if (collision.gameObject.tag == "Wall")
         {
-
             inAir = false;
             PlayerRigidbody.velocity = Vector3.zero;
             PlayerRigidbody.angularVelocity = 0;
             PlayerRigidbody.gravityScale = 0;
+        } else if (collision.gameObject.tag == "Swing")
+        {
+            inAir = false;
+            relativeJoint.enabled = true;
+            relativeJoint.connectedBody = collision.gameObject.GetComponent<Rigidbody2D>();
         }
     }
 }
