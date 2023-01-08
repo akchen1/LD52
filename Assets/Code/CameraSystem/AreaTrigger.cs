d4c51d594135924269288619991ddadd8218c75c
Gabriel Chan <gaibryol@gmail.com>
2023-01-07 17:24:01 -0700

    Added camera controls and better area manager stuff

---
+++ Assets/Code/CameraSystem/AreaTrigger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaTrigger : MonoBehaviour
+{
+	private AreaManager areaManager;
+	public int Area;
+
+	private void Awake()
+	{
+		areaManager = GameObject.FindGameObjectWithTag("AreaManager").GetComponent<AreaManager>();
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			areaManager.EnterArea(Area);
+		}
+	}
+}
